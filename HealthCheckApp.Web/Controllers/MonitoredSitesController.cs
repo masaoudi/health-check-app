f870e9981db6723477c12a94c313ef5fce15cc71
Amine Saoudi <amine.saoudi@ad.mediapost.fr>
2025-08-20 16:44:29 +0200

    FEAT: Added MonitoredSites Controller and Views with basic validation

---
+++ HealthCheckApp.Web/Controllers/MonitoredSitesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HealthCheckApp.Web.Models;
+
+namespace HealthCheckApp.Web.Controllers
+{
+    public class MonitoredSitesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MonitoredSitesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MonitoredSites
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MonitoredSites.ToListAsync());
+        }
+
+        // GET: MonitoredSites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monitoredSite = await _context.MonitoredSites
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (monitoredSite == null)
+            {
+                return NotFound();
+            }
+
+            return View(monitoredSite);
+        }
+
+        // GET: MonitoredSites/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MonitoredSites/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Url,Name,CheckIntervalSeconds,IsUp,LastChecked,LastDownTime,UserEmail")] MonitoredSite monitoredSite)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(monitoredSite);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(monitoredSite);
+        }
+
+        // GET: MonitoredSites/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monitoredSite = await _context.MonitoredSites.FindAsync(id);
+            if (monitoredSite == null)
+            {
+                return NotFound();
+            }
+            return View(monitoredSite);
+        }
+
+        // POST: MonitoredSites/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Url,Name,CheckIntervalSeconds,IsUp,LastChecked,LastDownTime,UserEmail")] MonitoredSite monitoredSite)
+        {
+            if (id != monitoredSite.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(monitoredSite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MonitoredSiteExists(monitoredSite.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(monitoredSite);
+        }
+
+        // GET: MonitoredSites/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var monitoredSite = await _context.MonitoredSites
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (monitoredSite == null)
+            {
+                return NotFound();
+            }
+
+            return View(monitoredSite);
+        }
+
+        // POST: MonitoredSites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var monitoredSite = await _context.MonitoredSites.FindAsync(id);
+            if (monitoredSite != null)
+            {
+                _context.MonitoredSites.Remove(monitoredSite);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MonitoredSiteExists(int id)
+        {
+            return _context.MonitoredSites.Any(e => e.Id == id);
+        }
+    }
+}
--- HealthCheckApp.Web/Models/MonitoredSite.cs
+++ HealthCheckApp.Web/Models/MonitoredSite.cs
@@ -1,10 +1,21 @@
-﻿namespace HealthCheckApp.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthCheckApp.Web.Models
 {
     public class MonitoredSite
     {
         public int Id { get; set; } // Clé primaire
+
+        [Required(ErrorMessage = "L'URL est obligatoire.")]
+        [Url(ErrorMessage = "Veuillez entrer une URL valide (commençant par http:// ou https://).")]
+        [Display(Name = "URL à surveiller")] // Change le label affiché dans la vue
         public string Url { get; set; } = string.Empty; // L'URL à surveiller
+       
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
+        [Display(Name = "Nom du site")]
         public string Name { get; set; } = string.Empty; // Un nom convivial donné par l'user
+
+        [Display(Name = "Intervalle de vérification (secondes)")]
         public int CheckIntervalSeconds { get; set; } = 600; // Par défaut: 10 min (600 sec)
 
         // Suivi de l'état
