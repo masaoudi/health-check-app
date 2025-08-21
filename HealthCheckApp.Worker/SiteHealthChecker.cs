63b7068dbd9ca328e03fa8d5cedf63eaa82124ec
Amine Saoudi <amine.saoudi@ad.mediapost.fr>
2025-08-21 19:44:18 +0200

    add space

--- HealthCheckApp.Worker/SiteHealthChecker.cs
+++ HealthCheckApp.Worker/SiteHealthChecker.cs
@@ -105,6 +105,7 @@ namespace HealthCheckApp.Worker
             public DateTime Last { get; set; }
             public DateTime Next { get; set; }
             public DateTime LastUpdated { get; set; }
+
         }
     }
 }
