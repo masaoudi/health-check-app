69f7782ada5dfa17c2124d560900fc84f146f285
Amine Saoudi <amine.saoudi@ad.mediapost.fr>
2025-08-21 12:20:56 +0200

    CONFIG: Updated connection strings for Azure SQL and deployment

--- HealthCheckApp.Web/Program.cs
+++ HealthCheckApp.Web/Program.cs
@@ -9,7 +9,14 @@ builder.Services.AddControllersWithViews();
 // Pour le développement, on utilise SQL Server LocalDB (inclus avec VS)
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5, // Nombre maximum de tentatives
+            maxRetryDelay: TimeSpan.FromSeconds(30), // Délai maximum entre chaque tentative
+            errorNumbersToAdd: null // Liste optionnelle d'erreurs supplémentaires à considérer comme temporaires
+        );
+    }));
 
 var app = builder.Build();
 
